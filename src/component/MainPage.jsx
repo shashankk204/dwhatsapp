fe2e1945602a5437f26291f990d2e9839d94df18
Shashank kumar <shashankk204@gmail.com>
2024-03-30 13:08:14 +0530

    minor bug fixed

--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -29,7 +29,7 @@ const FriendlistItemStyle = {
     justifyContent: 'center', // Center the text horizontally
     alignItems: 'center',    // Center the text vertically
     padding: '10px',         // Add padding to the ListItem
-  };
+};
 
 
 
@@ -45,8 +45,8 @@ const Friends = ({ provider, GetFriendList, OpenMessage }) => {
     const [frnTxt, SetfrnTxt] = useState("");
     const dis = useDispatch()
     const FriendList = useSelector(state => state.FriendList.value)
-    const [loading, setLoading] =useState(false);
-    const [disabled,setdisabled]=useState(false);
+    const [loading, setLoading] = useState(false);
+    const [disabled, setdisabled] = useState(false);
 
     const AddFriend = async () => {
         setLoading(true);
@@ -86,45 +86,44 @@ const Friends = ({ provider, GetFriendList, OpenMessage }) => {
                     <input type="text" className="p-1 rounded-md w-96" name="Friends address" placeholder='Friends address' value={frnTxt} onChange={(e) => { SetfrnTxt(e.target.value) }} />
                     {/* <button className="bg-blue-400 mx-3" onClick={AddFriend}>Add Friend</button> */}
 
-                    <LoadingButton 
-          onClick={AddFriend}
-          loading={loading}
-          loadingIndicator="Loading…"
-          variant="contained" 
-          disabled={disabled}
-          >
-          <span>Add Friend</span>
-        </LoadingButton>
+                    <LoadingButton
+                        onClick={AddFriend}
+                        loading={loading}
+                        loadingIndicator="Loading…"
+                        variant="contained"
+                        disabled={disabled}
+                    >
+                        <span>Add Friend</span>
+                    </LoadingButton>
                 </div>
             </div>
             <div className="flex-1 overflow-y-auto">
 
-               
-                    <List sx={styleFriendList} aria-label="mailbox folders">
-                        {(FriendList).map(
 
-                            (e) => {
-                                const k = Object.keys(e);
-                                const v = Object.values(e)
-                        
-                                return (<div  key={k}>
-                                    <ListItem data-value={k} 
+                <List sx={styleFriendList} aria-label="mailbox folders">
+                    {(FriendList).map(
+
+                        (e) => {
+                            const k = Object.keys(e);
+                            const v = Object.values(e)
+
+                            return (<div key={k}>
+                                <ListItem data-value={k}
                                     onClick={
-                                        async (e) => 
-                                        {
+                                        async (e) => {
                                             dis(SetActive(e.currentTarget.getAttribute('data-value')));
                                             await OpenMessage(e.currentTarget.getAttribute('data-value'));
                                         }
                                     }>
-                                        <ListItemText style={FriendlistItemStyle} primary={v} />
-                                    </ListItem>
-                                    <Divider component="li" />
-                                </div>
-                                )
-                            }
-                        )}
-                    </List>
-                
+                                    <ListItemText style={FriendlistItemStyle} primary={v} />
+                                </ListItem>
+                                <Divider component="li" />
+                            </div>
+                            )
+                        }
+                    )}
+                </List>
+
             </div>
 
         </div>
@@ -165,7 +164,7 @@ const Friends = ({ provider, GetFriendList, OpenMessage }) => {
 
 
 
-const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
+const Chats = ({ provider, TO, ConnectToWalletButtonHandler }) => {
 
     const [Messagetxt, SetMessagetxt] = useState("");
     const Address = useSelector(state => state.Address.value);
@@ -217,26 +216,26 @@ const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
 
                     if (receiver.toUpperCase()
                         == Address.toUpperCase() && Active.toUpperCase() == sender.toUpperCase()) {
-                            const signer = await provider.getSigner();
-                            const contract = new ethers.Contract(ContractAddress, abi, signer);
-                            const txResponse = await contract.GetMessage(sender);
-                            let n = Array.from(txResponse).length
-                            let arr = Array.from(txResponse)[n - 1];
-                            arr = Array.from(arr);
-                            let obj = {};
-                            obj["Text"] = arr[0];
-                            obj["sender"] = arr[1];
-                            obj["receiver"] = arr[2];
-                            obj["TypeOFMessage"] = Number(arr[3]);
-                            dis(SetallMessage(obj));
+                        const signer = await provider.getSigner();
+                        const contract = new ethers.Contract(ContractAddress, abi, signer);
+                        const txResponse = await contract.GetMessage(sender);
+                        let n = Array.from(txResponse).length
+                        let arr = Array.from(txResponse)[n - 1];
+                        arr = Array.from(arr);
+                        let obj = {};
+                        obj["Text"] = arr[0];
+                        obj["sender"] = arr[1];
+                        obj["receiver"] = arr[2];
+                        obj["TypeOFMessage"] = Number(arr[3]);
+                        dis(SetallMessage(obj));
                     }
                 }
                 )
             }
         )
     }, [Active]);
-    const [LoadingSend,setLoading]=useState(false);
-    const [DisabledSend,setDisabled]=useState(false);
+    const [LoadingSend, setLoading] = useState(false);
+    const [DisabledSend, setDisabled] = useState(false);
 
     const sendMessage = async () => {
         setLoading(true)
@@ -263,18 +262,18 @@ const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
                         <h1 className="bg-gray-200 sticky top-0 z-10 py-7 px-28 flex justify-between items-center" >
                             <span>
 
-                            {`${TO}`}
+                                {`${TO}`}
                             </span>
-                        <span>
+                            <span>
 
-                        <Nav ConnectToWalletButtonHandler={ConnectToWalletButtonHandler}></Nav>
-                        </span>
+                                <Nav ConnectToWalletButtonHandler={ConnectToWalletButtonHandler}></Nav>
+                            </span>
                         </h1>
 
-                        <div className="flex-1 overflow-y-auto">
+                        <div className="flex-1 overflow-y-auto flex flex-col-reverse">
                             {allMessage.map((e) => {
                                 const isMessageFromActiveUser = (e.sender.toUpperCase() === Active.toUpperCase());
-                                const messageClass = isMessageFromActiveUser ? "justify-end" : "justify-start";
+                                const messageClass = !isMessageFromActiveUser ? "justify-end" : "justify-start";
                                 return (
                                     <div className={`flex ${messageClass} mb-2`} key={e.key}>
 
@@ -287,7 +286,7 @@ const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
                                             </div>
                                         </div>
                                     </div>
-                                
+
                                 )
                             })}
                         </div>
@@ -296,15 +295,15 @@ const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
                             <input type="text" placeholder='Type your Message' className='w-96 px-2 py-3 rounded-3xl' value={Messagetxt} onChange={(e) => { SetMessagetxt(e.target.value) }} />
                             {/* <button className="bg-blue-800" onClick={sendMessage}>Send</button> */}
                             <LoadingButton
-          onClick={sendMessage}
-          endIcon={<SendIcon />}
-          loading={LoadingSend}
-          loadingPosition="end"
-          variant="contained"
-          disabled={DisabledSend}
-        >
-          <span>Send</span>
-        </LoadingButton>
+                                onClick={sendMessage}
+                                endIcon={<SendIcon />}
+                                loading={LoadingSend}
+                                loadingPosition="end"
+                                variant="contained"
+                                disabled={DisabledSend}
+                            >
+                                <span>Send</span>
+                            </LoadingButton>
                         </div>
                     </div>
                 </>)}
@@ -335,7 +334,7 @@ const Chats = ({ provider, TO ,ConnectToWalletButtonHandler}) => {
 
 
 
-const MainPage = ({ provider, GetFriendList, To, setTO ,ConnectToWalletButtonHandler}) => {
+const MainPage = ({ provider, GetFriendList, To, setTO, ConnectToWalletButtonHandler }) => {
 
     const dis = useDispatch()
 
